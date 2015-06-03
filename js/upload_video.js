bf7718f51c639797a3c3e3053f13c8b5b785cd04
vkolova <onlysilence@abv.bg>
2015-06-04 00:30:31 +0300

    Finishing touches..

--- js/upload_video.js
+++ js/upload_video.js
@@ -24,7 +24,7 @@ function loadUpload() {
 									  '<option>private</option>' +
 									'</select>');
 
-	$('#upload-container').append('<input input type="file" class="btn btn-default" id="file" value="Submit" accept="video/*">');
+	$('#upload-container').append('<input input type="file" class="btn btn-default" id="file" accept="video/*">');
 	$('#upload-container').append('<button class="btn btn-default" id="button">Upload Video</button>');
 	
 	
