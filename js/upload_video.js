438071fdb7e324a92ce5fae3c926dc8297117fcb
vkolova <onlysilence@abv.bg>
2015-06-04 00:29:06 +0300

    Finishing touches..

--- js/upload_video.js
+++ js/upload_video.js
@@ -15,14 +15,18 @@ function loadUpload() {
 	$('#upload-container').append('<input type="text" class="form-control" id="title" placeholder="Title">');
 	$('#upload-container').append('<textarea class="form-control" id="description" rows="3" placeholder="Description"></textarea>');
 	
-	$('#upload-container').append('<select class="form-control" style="width: 25%;">' +
+	//$('#upload-container').append('<label for="privacy-status">Privacy Status:</label><select id="privacy-status"><option>public</option><option>unlisted</option><option>private</option></select>');
+	//$('#upload-container').append('<input input type="file" id="file" class="button" accept="video/*"><button id="button">Upload Video</button>');
+
+	$('#upload-container').append('<select class="form-control" for="privacy-status" style="width: 25%;">' +
 									  '<option>public</option>' +
 									  '<option>unlisted</option>' +
 									  '<option>private</option>' +
 									'</select>');
 
-	$('#upload-container').append('<input class="btn btn-default" id="file" type="file" value="Submit" accept="video/*">');
+	$('#upload-container').append('<input input type="file" class="btn btn-default" id="file" value="Submit" accept="video/*">');
 	$('#upload-container').append('<button class="btn btn-default" id="button">Upload Video</button>');
+	
 	
 	uploadVideo();
 }
